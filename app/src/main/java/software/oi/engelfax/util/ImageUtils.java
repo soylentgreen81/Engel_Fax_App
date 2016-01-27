c6591e76222c1f43c0aa2af07a45f7f4f7319029
soylentgreen81 <Cornholio81>
2016-01-27 22:31:21 +0100

    - Fehler in im Resizing behoben

--- app/src/main/java/software/oi/engelfax/util/ImageUtils.java
+++ app/src/main/java/software/oi/engelfax/util/ImageUtils.java
@@ -17,7 +17,7 @@ public abstract class ImageUtils {
     public static Bitmap scaleMonochrome(Context context, Uri imagepath, final int width, final int height){
         try {
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imagepath);
-            Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, 24, 24, false);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
             scaledBitmap.setHasAlpha(false);
             for (int y=0;y<height;y++){
                 for (int x=0;x<width;x++){
